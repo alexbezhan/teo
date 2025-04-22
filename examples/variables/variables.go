f8cda9e06764adcb33664d45ad77e441157789d5
Alex Bezhan <alex.bezhan@amspilot.net>
2025-04-22 19:07:34 +0100

    fake formatting

--- examples/variables/variables.go
+++ examples/variables/variables.go
@@ -3,13 +3,16 @@ package main
 import "fmt"
 
 func main() {
-var a int
-a = 1
-fmt.Println(a)
-b := 2
-fmt.Println(b)
-str := "Hello World"
-fmt.Println(str)
-str2 := "Hello World2"
-fmt.Println(str2)
+	var a int
+	a = 1
+	fmt.Println(a)
+
+	b := 2
+	fmt.Println(b)
+
+	str := "Hello World"
+	fmt.Println(str)
+
+	str2 := "Hello World2"
+	fmt.Println(str2)
 }
